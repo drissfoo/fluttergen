68032a4696654be567e4de35c2e593562f59a9c6
drissboo <driss.bounouar@igraal.com>
2023-11-29 14:28:24 +0100

    Try to bind swift classes

---
+++ ios/Runner/ExampleSwift.swift
@@ -0,0 +1,14 @@
+//
+//  ExampleSwift.swift
+//  Runner
+//
+//  Created by Driss Bounouar on 29/11/2023.
+//
+
+import Foundation
+
+@objc public class SwiftClass: NSObject {
+    @objc public func sum(_ a: Int, _ b: Int) -> Int {
+        return a + b + 3
+    }
+}
--- lib/sum_api.dart
+++ lib/sum_api.dart
@@ -5,14 +5,14 @@ import 'dart:io';
 
 import 'package:fluttergen/api_bindings.dart';
 import 'package:fluttergen/example.dart';
+import 'package:fluttergen/swift_api_bindings.dart';
 
 int sum(int a, int b){
   if(Platform.isAndroid){
     return Example.sum(a, b);
   }else{
-    final lib = SwiftLibrary(DynamicLibrary.process());
-    final exa = ExampleObjc.alloc(lib);
-
-    return ExampleObjc.add_to_(lib, a, b);
+    final lib = SwiftLibrary(DynamicLibrary.open('/Users/drissfoo/Documents/dev/gsg/mobile/flutter/flutterProjects/fluttergen/ios/Runner/third_party/libswiftapi.dylib'));
+    //final lib = ObjcLibrary(DynamicLibrary.process());
+    return SwiftClass.new1(lib).sum__(a, b);
   }
 }
